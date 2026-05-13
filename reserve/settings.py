303c32a42efa5ece0a6d6df6d0ec5997a9629b86
Tomas Matis <tomasbelaxinen@gmail.com>
2026-05-13 09:16:54 +0200

    -e

--- reserve/settings.py
+++ reserve/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n1a#tojb!!u$=_^pf+%&q%if2^wx!=e)i+9s2cv1qx_joleji3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["matis-pi13-projekt.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
